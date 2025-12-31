aa9dd521cfa54af5c2905f49cc83677985523af3
sarthakzzzzz <sarthakpujari1970@gmail.com>
2026-01-01 01:19:49 +0530

    All Parts Uploaded

--- part3.py
+++ part3.py
@@ -2,14 +2,22 @@ from flask import Flask, request, jsonify
 import sqlalchemy
 import os
 app = Flask(__name__)
-# connect postgres database
-DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://postgres:<user_password>@localhost:5432/inventory_db')
+DATABASE_URI = os.environ.get(
+    'DATABASE_URL', 'postgresql+psycopg2://postgres:<user_password>@localhost:5432/inventory_db')
+
+# used sqlalchemy to execute query using postgresql database its ordbms and sqlalchemy is orm for python
 engine = sqlalchemy.create_engine(DATABASE_URI)
 
 
 @app.route('/api/companies/<int:company_id>/alerts/low-stock', methods=['GET'])
 def get_low_stock(company_id):
+    # connect to the database using the engine
     with engine.connect() as connection:
+        # --- SQL Query for Low Stock Products ---
+        # This query joins the inventory, product, warehouse, and supplier tables
+        # to gather all necessary information for the low stock alerts.
+        # just check if current_sstock is less than or equal to threshold this will give low stock products
+        # after that return the result by taking length of alerts
         query = """
         SELECT 
             p.id AS product_id,
@@ -19,6 +27,7 @@ def get_low_stock(company_id):
             w.name AS warehouse_name,
             i.quantity AS current_stock,
             i.threshold,
+            -- Build a JSON object for supplier details to nest the information.
             json_build_object(
                 'id', s.id,
                 'name', s.name,
@@ -32,7 +41,9 @@ def get_low_stock(company_id):
         """
         result = connection.execute(sqlalchemy.text(query), {
                                     "company_id": company_id})
+
         alerts = [dict(row._mapping) for row in result]
+
         return jsonify({'alerts': alerts, 'total_alerts': len(alerts)}), 200
 
 
